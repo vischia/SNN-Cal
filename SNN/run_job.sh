b4d63aa7899fd6fda99918ca283ce330bbdad0b2
Pietro Vischia <pietro.vischia@gmail.com>
2026-03-25 14:58:18 +0100

    Create logs directory if not existing

--- SNN/run_job.sh
+++ SNN/run_job.sh
@@ -4,5 +4,5 @@ source /lhome/ext/uovi123/`whoami`/.bashrc
 conda activate snn_hgcal
 # Adapt to the path where you installed it:
 cd /lhome/ext/uovi123/`whoami`/snn_calo/SNN-Cal/
-
+mkdir -p logs
 python $1
