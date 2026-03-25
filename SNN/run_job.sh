b434de08eb27c1c6c4cb67941b5bdea67a90819b
Pietro Vischia <pietro.vischia@gmail.com>
2026-03-25 11:09:19 +0100

    Added a note about each user having to edit the run_job.sh

--- SNN/run_job.sh
+++ SNN/run_job.sh
@@ -2,6 +2,7 @@
 
 source /lhome/ext/uovi123/`whoami`/.bashrc
 conda activate snn_hgcal
-cd /lhome/ext/uovi123/`whoami`/SNN-Cal/
+# Adapt to the path where you installed it:
+cd /lhome/ext/uovi123/`whoami`/snn_calo/SNN-Cal/
 
 python $1
